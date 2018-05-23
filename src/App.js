487dd154dd8a29c573d98976416609bbea304269
mudroljub <mudroljub@gmail.com>
2018-05-23 17:42:45 +0200

    radovi

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import Developer from './components/Developer'
-import developers from './data/developers.json'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      developers: []
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ loading: true })
+    fetch('https://raw.githubusercontent.com/skolakoda/nadji-developera/master/src/data/developers.json')
+    .then(data => data.json())
+    .then(data => this.setState({
+      developers: data,
+      loading: false
+    }))
+  }
+
   render() {
-    const devList = developers.map((dev, i) => 
+    const devList = this.state.developers.map((dev, i) => 
       <Developer name={dev.name} skills={dev.skills} image={dev.image} key={i} />
     )
     return (
       <div className="App">
-
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Nađi developera</h1>
         </header>
 
         <div className="flex-wrapper">
-          {devList}
+          {this.state.loading ? "Loading..." : devList}
         </div>
       </div>
-
     );
   }
 }
--- src/components/Developer.js
+++ src/components/Developer.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './developer.css'
 
-export default class Developer extends Component {
-  render() {
-    return (
-      <div className="dev-wrapper">
-        <h3 className="dev-name">{this.props.name}</h3>
-        <img alt="developer" src={this.props.image} />
-        <p>Moje glavne veštine su {this.props.skills}.</p>
-      </div>
-    )
-  }
-}
+// export default class Developer extends Component {
+//   render() {
+//     return (
+//       <div className="dev-wrapper">
+//         <h3 className="dev-name">{this.props.name}</h3>
+//         <img alt="developer" src={this.props.image} />
+//         <p>Moje glavne veštine su {this.props.skills}.</p>
+//       </div>
+//     )
+//   }
+// }
+
+const Developer = props => (
+  <div className="dev-wrapper">
+    <h3 className="dev-name">{props.name}</h3>
+    <img alt="developer" src={props.image} />
+    <p>Moje glavne veštine su {props.skills}.</p>
+  </div>
+)
+
+export default Developer
