0e9eb1e7d003bcf0c02831b7a7ab61ffb9daf615
mudroljub <mudroljub@gmail.com>
2018-07-30 11:43:30 +0200

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import {setLoading, setDevelopers, setFiltered} from './store/actions'
 import Header from './components/Header'
